07bb2730023f79c9289d049e990e2ea89caa5e2e
Rishabh Gist <damyrachit@gmail.com>
2022-10-29 02:20:46 +0530

    added highest and lowest variable assigned with scores

--- src/main/java/com/rgc/BreakingTheRecord.java
+++ src/main/java/com/rgc/BreakingTheRecord.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class BreakingTheRecord {
     public static List<Integer> breakingRecords(List<Integer> scores) {
+        // Storing first value in highest and lowest
+        int highest = scores.get(0);
+        int lowest = scores.get(0);
 
     }
 }
